0370118c6335994415a34ab1f4e8489d2e6d2d86
avasiaxx <68916907+avasiaxx@users.noreply.github.com>
2023-10-11 12:50:15 -0400

    Login TextView now formatted with correct color palette

--- app/src/main/java/com/avasia/reeltalk/welcomescreens/WelcomeDetailsFragment.kt
+++ app/src/main/java/com/avasia/reeltalk/welcomescreens/WelcomeDetailsFragment.kt
@@ -1,11 +1,17 @@
 package com.avasia.reeltalk.welcomescreens
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.avasia.reeltalk.R
+import com.avasia.reeltalk.databinding.FragmentWelcomeDetailsBinding
 
 class WelcomeDetailsFragment: Fragment() {
     override fun onCreateView(
@@ -13,6 +19,49 @@ class WelcomeDetailsFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome_details, container, false)
+        return inflater.inflate(
+            R.layout.fragment_welcome_details,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(
+            view,
+            savedInstanceState
+        )
+
+        /* Get the login textview string, format it with the correct color palette using
+        ForegroundColorSpans & a spannableStringBuilder */
+        val binding = FragmentWelcomeDetailsBinding.bind(view)
+        val loginString: String =
+            binding.root.context.resources.getString(
+                R.string.already_have_an_account_login
+            )
+        val colorSpan1 = ForegroundColorSpan(
+            ContextCompat.getColor(
+                binding.root.context, R.color.highEmphasis
+            )
+        )
+        val colorSpan2 = ForegroundColorSpan(
+            ContextCompat.getColor(
+                binding.root.context, R.color.colorPrimary
+            )
+        )
+        val spannable = SpannableStringBuilder(loginString)
+        spannable.setSpan(
+            colorSpan1,
+            0,
+            24,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            colorSpan2,
+            25,
+            30,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.login.text = spannable
     }
 }
