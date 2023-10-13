ccf54f20af31ec13981ab921ff2c60bf66de46b0
avasiaxx <68916907+avasiaxx@users.noreply.github.com>
2023-10-13 14:29:34 -0400

    Initial Welcome Fragment documentation added

--- app/src/main/java/com/avasia/reeltalk/welcomescreens/InitialWelcomeFragment.kt
+++ app/src/main/java/com/avasia/reeltalk/welcomescreens/InitialWelcomeFragment.kt
@@ -9,7 +9,35 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.avasia.reeltalk.R
-
+/**
+ * Fragment for the initial welcome screen with a brief delay before transitioning to the splash screen.
+ *
+ * The `InitialWelcomeFragment` is the first screen users encounter when they launch the app. It serves
+ * as a brief welcome or branding screen and includes a short delay before automatically navigating to
+ * the splash screen. This delay provides a smooth introduction to the app and allows time for any
+ * necessary initialization.
+ *
+ * Usage:
+ * - Include this fragment as the initial screen of your app's navigation flow.
+ * - Customize the appearance and content of this welcome screen to create a welcoming first impression
+ *   for users.
+ * - Adjust the `SPLASH_DELAY` constant to control the duration of the delay before transitioning to the
+ *   splash screen.
+ *
+ * Example:
+ * ```
+ * val initialWelcomeFragment = InitialWelcomeFragment()
+ * supportFragmentManager.beginTransaction()
+ *     .replace(R.id.fragment_container, initialWelcomeFragment)
+ *     .commit()
+ * ```
+ *
+ * Note:
+ * - While this fragment provides a seamless entry into the app, ensure that the delay is kept minimal to
+ *   prevent user impatience.
+ * - For custom branding and introduction, consider customizing the layout and content in the associated
+ *   XML layout file.
+ */
 class InitialWelcomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
