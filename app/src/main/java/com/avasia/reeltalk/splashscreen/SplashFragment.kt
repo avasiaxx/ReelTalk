699dbf4412f7ccafa103152fbb954e4a88ea0553
avasiaxx <68916907+avasiaxx@users.noreply.github.com>
2023-10-13 14:25:47 -0400

    Splash screen documentation added

--- app/src/main/java/com/avasia/reeltalk/splashscreen/SplashFragment.kt
+++ app/src/main/java/com/avasia/reeltalk/splashscreen/SplashFragment.kt
@@ -9,7 +9,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.avasia.reeltalk.R
-
+/**
+ * Splash Screen for the "Reel Talk" app.
+ *
+ * This splash screen is the initial screen that users see when they launch the "Reel Talk" app.
+ * It is designed to provide a visually appealing and branded introduction to the app. The splash
+ * screen typically displays the app's logo and name, creating a memorable first impression for
+ * users.
+ */
 class SplashFragment : Fragment() {
 
     override fun onCreateView(
