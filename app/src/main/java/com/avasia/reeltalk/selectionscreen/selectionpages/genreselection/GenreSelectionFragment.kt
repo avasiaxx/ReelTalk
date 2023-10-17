05e89953cea629a155ac8762ecab99de97fc21e6
avasiaxx <68916907+avasiaxx@users.noreply.github.com>
2023-10-17 13:52:54 -0400

    Movie/TV Genre Selection implemented

--- app/src/main/java/com/avasia/reeltalk/selectionscreen/selectionpages/genreselection/GenreSelectionFragment.kt
+++ app/src/main/java/com/avasia/reeltalk/selectionscreen/selectionpages/genreselection/GenreSelectionFragment.kt
@@ -13,6 +13,22 @@ import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipDrawable
 import com.google.android.material.chip.ChipGroup
 
+/**
+ * Fragment for selecting genres using Chips in an Android application.
+ * This fragment allows users to choose from a list of genres presented as Chips.
+ * The selected genres are displayed at the top, and unselected genres can be disabled.
+ * It interacts with the `GenreSelectionViewModel` to manage selected genres and provides
+ * a user-friendly interface for genre selection.
+ *
+ * This class handles the following key features:
+ * - Displaying a list of selectable genres as Chips.
+ * - Updating the list of selected genres as Chips are selected or deselected.
+ * - Disabling unselected genres when a maximum selection limit is reached.
+ *
+ * @property chipGroup A `ChipGroup` for displaying and managing the genre Chips.
+ * @property genreSelectionViewModel The associated ViewModel for genre selection logic.
+ * @property _binding A reference to the binding for the Fragment layout.
+ */
 
 class GenreSelectionFragment : Fragment() {
 
@@ -26,53 +42,96 @@ class GenreSelectionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(com.avasia.reeltalk.R.layout.fragment_genre_selection,
+        val view = inflater.inflate(
+            com.avasia.reeltalk.R.layout.fragment_genre_selection,
             container,
-            false)
+            false
+        )
         _binding = FragmentGenreSelectionBinding.bind(view)
         chipGroup = binding.chipGroup
         addChipsToGroup()
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        genreSelectionViewModel.selected.observe(viewLifecycleOwner) {
+            val amountSelected = genreSelectionViewModel.selected.value
+            val newSelectionTest = "$amountSelected${binding.selected.text.substring(1)}"
+            binding.selected.text = newSelectionTest
+
+            if (genreSelectionViewModel.checkIfMaxSelected()) {
+                disableUnselectedChips()
+            } else {
+                enableUnselectedChips()
+            }
+        }
+    }
+
     private fun addChipsToGroup() {
         for (text in genreSelectionViewModel.genreOptions) {
-            val chip = Chip(requireContext())
-            chip.text = text
-            val chipDrawable = ChipDrawable.createFromAttributes(
-                requireContext(),
-                null,
-                0,
-                com.avasia.reeltalk.R.style.ChipStyle
-            )
-            chip.setChipDrawable(chipDrawable)
-            chip.setPadding(110, 60, 110, 60)
-
-            chip.isCheckable = true
-            chip.isChecked = false
-
-            chip.setTextColor(ContextCompat.getColor(requireContext(),
-                com.avasia.reeltalk.R.color.highEmphasis)
-            )
-            chip.setChipBackgroundColorResource(com.avasia.reeltalk.R.color.thirdSurface)
-
-            // Handle Chip click events if needed
-            chip.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    chip.setTextColor(ContextCompat.getColor(requireContext(),
-                        com.avasia.reeltalk.R.color.thirdSurface)
-                    )
-                    chip.setChipBackgroundColorResource(com.avasia.reeltalk.R.color.highEmphasis)
-                } else {
-                    chip.setTextColor(ContextCompat.getColor(requireContext(),
-                        com.avasia.reeltalk.R.color.highEmphasis)
-                    )
-                    chip.setChipBackgroundColorResource(com.avasia.reeltalk.R.color.thirdSurface)
-                }
-            }
+            val chip = createConfiguredChip(text)
             chipGroup.addView(chip)
             chipGroup.setChipSpacing(1)
         }
+    }
+
+    private fun setChipsEnabled(isEnabled: Boolean) {
+        for (i in 0 until chipGroup.childCount) {
+            val chip = chipGroup.getChildAt(i) as Chip
+            if (!chip.isChecked) {
+                chip.isEnabled = isEnabled
+            }
+        }
+    }
+
+    private fun disableUnselectedChips() {
+        setChipsEnabled(false)
+    }
+
+    private fun enableUnselectedChips() {
+        setChipsEnabled(true)
+    }
+
+    private fun createConfiguredChip(text: String): Chip {
+        val chip = Chip(requireContext())
+        chip.text = text
+        val chipDrawable = ChipDrawable.createFromAttributes(
+            requireContext(),
+            null,
+            0,
+            com.avasia.reeltalk.R.style.ChipStyle
+        )
+        chip.setChipDrawable(chipDrawable)
+        chip.setPadding(110, 60, 110, 60)
+
+        chip.isCheckable = true
+        chip.isChecked = false
+
+        val highEmphasisColor = ContextCompat.getColor(
+            requireContext(),
+            com.avasia.reeltalk.R.color.highEmphasis
+        )
+        val thirdSurfaceColor = ContextCompat.getColor(
+            requireContext(),
+            com.avasia.reeltalk.R.color.thirdSurface
+        )
+
+        chip.setTextColor(highEmphasisColor)
+        chip.setChipBackgroundColorResource(com.avasia.reeltalk.R.color.thirdSurface)
+
+        chip.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                chip.setTextColor(thirdSurfaceColor)
+                chip.setChipBackgroundColorResource(com.avasia.reeltalk.R.color.highEmphasis)
+                genreSelectionViewModel.onIncrease()
+            } else {
+                chip.setTextColor(highEmphasisColor)
+                chip.setChipBackgroundColorResource(com.avasia.reeltalk.R.color.thirdSurface)
+                genreSelectionViewModel.onDecrease()
+            }
+        }
+        return chip
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/avasia/reeltalk/selectionscreen/selectionpages/genreselection/GenreSelectionViewModel.kt
+++ app/src/main/java/com/avasia/reeltalk/selectionscreen/selectionpages/genreselection/GenreSelectionViewModel.kt
@@ -1,8 +1,24 @@
 package com.avasia.reeltalk.selectionscreen.selectionpages.genreselection
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+/**
+ * ViewModel for managing genre selection in an Android application.
+ *
+ * This ViewModel is responsible for managing the selection of genres. It keeps track of the
+ * number of genres selected, enforces selection limits, and provides a list of available genre options.
+ *
+ * @property selected A LiveData representing the number of genres currently selected.
+ * @property genreOptions An array of available genre options.
+ */
 class GenreSelectionViewModel : ViewModel() {
+
+    private val _selected = MutableLiveData<Int>()
+
+    val selected: LiveData<Int>
+        get() = _selected
 
     val genreOptions = arrayOf(
         "Action",
@@ -26,4 +42,50 @@ class GenreSelectionViewModel : ViewModel() {
         "War",
         "Western"
     )
+
+    /**
+     * Initializes the ViewModel by setting the initial selected value to the minimum.
+     */
+    init {
+        _selected.value = MIN_SELECTED
+    }
+
+    /**
+     * Increases the selected count if it is below the maximum limit.
+     */
+    fun onIncrease() {
+        if (_selected.value!! < MAX_SELECTED) {
+            _selected.value = _selected.value!! + 1
+        }
+    }
+
+    /**
+     * Decreases the selected count if it is above the minimum limit.
+     */
+    fun onDecrease() {
+        if (_selected.value!! > 0) {
+            _selected.value = _selected.value!! - 1
+        }
+    }
+
+    /**
+     * Checks if the maximum selection limit has been reached.
+     *
+     * @return `true` if the maximum limit has been reached, `false` otherwise.
+     */
+    fun checkIfMaxSelected(): Boolean {
+        return _selected.value!! == MAX_SELECTED
+    }
+
+    companion object {
+        /**
+         * The maximum number of genres that can be selected.
+         */
+        const val MAX_SELECTED = 3
+
+        /**
+         * The minimum number of genres that can be selected.
+         */
+        const val MIN_SELECTED = 0
+    }
 }
